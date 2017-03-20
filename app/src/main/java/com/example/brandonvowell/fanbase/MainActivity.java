72a08e8c6c76588e19fa13c756d65b3db32bc0b7
ncerice <nacerice@crimson.ua.edu>
2017-03-20 12:37:21 -0500

    Added account registration screen

--- app/src/main/java/com/example/brandonvowell/fanbase/MainActivity.java
+++ app/src/main/java/com/example/brandonvowell/fanbase/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.brandonvowell.fanbase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //TODO
+        //If user is authenticated, start menu activity.
+        //If user is not authenticated, start login activity.
         setContentView(R.layout.activity_login);
+
+        final Button btnRegister = (Button) findViewById(R.id.btnRegister);
+        final Button btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextScreen = new Intent(v.getContext(), RegisterScreen.class);
+                startActivityForResult(nextScreen, 0);
+            }
+        });
     }
 }
