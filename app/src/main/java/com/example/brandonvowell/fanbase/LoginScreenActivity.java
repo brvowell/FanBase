fff6f8f5c74173247acd5ffa0101c3d06074985d
ncerice <nacerice@crimson.ua.edu>
2017-03-20 14:21:59 -0500

    Added some authentication code

--- app/src/main/java/com/example/brandonvowell/fanbase/LoginScreenActivity.java
+++ app/src/main/java/com/example/brandonvowell/fanbase/LoginScreenActivity.java
@@ -2,17 +2,27 @@ package com.example.brandonvowell.fanbase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginScreenActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "LoginScreenActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
         final Button btnRegister = (Button) findViewById(R.id.btnRegister);
         final Button btnLogin = (Button) findViewById(R.id.btnLogin);
 
@@ -23,5 +33,33 @@ public class LoginScreenActivity extends AppCompatActivity {
                 startActivityForResult(nextScreen, 0);
             }
         });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
