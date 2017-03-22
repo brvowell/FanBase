78436d130b8fe81ee277792b1d6e3fd0e2903836
ncerice <nacerice@crimson.ua.edu>
2017-03-22 14:13:10 -0500

    fixed crash on loading main activity

--- app/src/main/java/com/example/brandonvowell/fanbase/MainActivity.java
+++ app/src/main/java/com/example/brandonvowell/fanbase/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         //TODO
         //If user is authenticated, start menu activity.
         //If user is not authenticated, start login activity.
