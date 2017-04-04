96dd857f78291f13dfe59e38a53aec7cfe8fc7be
ncerice <nacerice@crimson.ua.edu>
2017-04-03 20:09:59 -0500

    Initial progress on menu/app layout

---
+++ app/src/main/java/com/example/brandonvowell/fanbase/SettingsActivity.java
@@ -0,0 +1,30 @@
+package com.example.brandonvowell.fanbase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+}
