7fb768bd31833577d062632f969e686b43b7ec45
brvowell <brvowell@crimson.ua.edu>
2017-04-06 15:20:19 -0500

    Added Images and Updated UI

--- app/src/main/java/com/example/brandonvowell/fanbase/MainActivity.java
+++ app/src/main/java/com/example/brandonvowell/fanbase/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.brandonvowell.fanbase;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Toolbar my_toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        //setSupportActionBar(my_toolbar);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
@@ -55,20 +58,19 @@ public class MainActivity extends AppCompatActivity {
     private void setupMenuScreenListeners() {
         mAuth = FirebaseAuth.getInstance();
 
-        final Button btnLogout = (Button) findViewById(R.id.btnLogout);
         final Button btnCreate = (Button) findViewById(R.id.btnCreate);
         final Button btnBrowse = (Button) findViewById(R.id.btnBrowse);
         final Button btnSettings = (Button) findViewById(R.id.btnSettings);
         final Button btnHistory = (Button) findViewById(R.id.btnHistory);
 
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+        /*btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //logout of firebase instance, go back to login activity
                 mAuth.signOut();
                 transitionLoginScreenActivity();
             }
-        });
+        }); */
 
         btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
