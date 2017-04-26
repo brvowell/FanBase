76074df3aab88744767bf7d84289cf61397d66fb
ncerice <nacerice@crimson.ua.edu>
2017-04-26 18:34:05 -0500

    tailgate page appearance tweaks

--- app/src/main/java/com/example/brandonvowell/fanbase/TailgateDetailActivity.java
+++ app/src/main/java/com/example/brandonvowell/fanbase/TailgateDetailActivity.java
@@ -16,6 +16,14 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
@@ -26,7 +34,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class TailgateDetailActivity extends FragmentActivity {
+import im.delight.android.location.SimpleLocation;
+
+public class TailgateDetailActivity extends FragmentActivity implements OnMapReadyCallback {
 
     Tailgate currentTailgate;
     FirebaseStorage storage;
@@ -34,10 +44,23 @@ public class TailgateDetailActivity extends FragmentActivity {
     public LinearLayout layout;
     ImageView coverPhotoView;
 
+    private GoogleMap mMap;
+
+    private SimpleLocation location;
+
+
+    private double latitude;
+    private double longitude;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tailgate_detail);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.detail_map);
+        mapFragment.getMapAsync(this);
+
         TextView tailgateName = (TextView) findViewById(R.id.tailgate_name_textview);
         TextView tailgateDescription = (TextView) findViewById(R.id.description_textview);
         TextView thingsToBring = (TextView) findViewById(R.id.thingsToBring_textView);
@@ -47,6 +70,8 @@ public class TailgateDetailActivity extends FragmentActivity {
 
         currentTailgate = (Tailgate) getIntent().getSerializableExtra("TAILGATE");
         storage = FirebaseStorage.getInstance();
+
+        location = new SimpleLocation(this);
 
         String name = currentTailgate.tailgateName;
         String description = currentTailgate.tailgateDescription;
@@ -132,5 +157,40 @@ public class TailgateDetailActivity extends FragmentActivity {
 
         imageDialog.create();
         imageDialog.show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+        getCurrentDeviceLocation();
+        LatLng myLocation = new LatLng(this.latitude, this.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 17.0f));
+        LatLng pinLocation = new LatLng(currentTailgate.latitude, currentTailgate.longitude);
+        BitmapDescriptor pinColor;
+        if (currentTailgate.tailgateIsHome == 1) {
+            pinColor = BitmapDescriptorFactory.defaultMarker(12.00f);
+        } else {
+            pinColor = BitmapDescriptorFactory.defaultMarker(231.00f);
+        }
+        mMap.addMarker(new MarkerOptions()
+                .position(pinLocation).title(currentTailgate.startTime).icon(pinColor)).setTag(currentTailgate);
+    }
+
+    private void getCurrentDeviceLocation() {
+        this.latitude = location.getLatitude();
+        this.longitude = location.getLongitude();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        location.beginUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        location.endUpdates();
+        super.onPause();
     }
 }
